68b6202e0f158494895915f130807862a863ba1c
andresaugusto <andres.augusto@icloud.com>
2021-10-11 20:28:48 -0400

    fixed directory bug and HomeBackground bug

---
+++ build/precache-manifest.0f90b66d6f4de08084b8cc3adb4facc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef8942dc783807696b103eb423f76f0",
+    "url": "/Br/index.html"
+  },
+  {
+    "revision": "a943fb1db0226b1e7299",
+    "url": "/Br/static/css/main.be45d3b8.chunk.css"
+  },
+  {
+    "revision": "2f580cb69940731acca2",
+    "url": "/Br/static/js/2.d6b1c5a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Br/static/js/2.d6b1c5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a943fb1db0226b1e7299",
+    "url": "/Br/static/js/main.63a8d257.chunk.js"
+  },
+  {
+    "revision": "6510fa81a95a7062792b",
+    "url": "/Br/static/js/runtime-main.451e2783.js"
+  }
+]);
